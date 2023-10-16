905ea91d12d0d85c84f157a919a3969ad546d6f4
unknown <raufyl37@gmail.com>
2023-10-16 05:12:47 +0300

    GridSystem

---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    public static UnitActionSystem instance;
+
+    public event Action OnSelectedUnitChanged;
+
+    [field: SerializeField] public Unit SelectedUnit { get; private set; }
+    [SerializeField] private LayerMask unitsLayerMask;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (TryHandleUnitSelection()) return;
+            if(SelectedUnit != null)
+            {
+                SelectedUnit.Move(MouseWorld.GetPosition());
+            }
+        }
+    }
+    
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitsLayerMask)) 
+        {
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                SetSelectedUnit(unit);
+                return true;
+            }
+        }
+        return false;
+    }
+    private void SetSelectedUnit(Unit unit)
+    {
+        SelectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    private MeshRenderer meshRenderer;
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        UpdateVisual();
+        UnitActionSystem.instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged()
+    {
+        UpdateVisual();
+    }
+    private void UpdateVisual()
+    {
+        if (UnitActionSystem.instance.SelectedUnit == unit)
+        {
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
